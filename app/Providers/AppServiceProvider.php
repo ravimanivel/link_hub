070583e03d0788e8f78029cb05fa13cf4c4b0002
RAVI <69808819+ravimanivel@users.noreply.github.com>
2025-05-15 15:39:06 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-    if (env('APP_ENV') !== 'local') {
+    if (env('APP_ENV') !== 'production') {
         URL::forceScheme('https');
     }
     }
